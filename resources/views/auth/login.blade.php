386f4bdd9607a1f6428a697270ff7316c87a78c3
Roi Macuana <roi.macuana@republisys.com>
2017-03-08 09:02:34 +0800

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,8 +4,7 @@
     /*GLOBAL CSS*/
 
     body {
-        background:#101010 !important;
-
+        background: #ffffff!important;
     }
 
     .navbar-default {
@@ -45,10 +44,37 @@
                 margin: auto;
             }
                 .img-logo-company {
-                    height: 151px;
-                    width: 324px;
+                    height: 75px;
+                    width: 168px;
                     border: 1px solid;
                     margin: auto;
+                    position: relative;
+                    bottom: 33px;
+                }
+
+                .bdre2 {
+                    border-bottom: 2px solid #999;
+                    height: 1px;
+                    margin: 6px auto;
+                    width: 100px;
+                    margin-bottom: 30px;
+                }
+
+                .title {
+                    font-size: 26px;
+                    color: #393939;
+                    margin: auto;
+                    text-align: center;
+                    font-weight: 300;
+                }
+
+                .title2 {
+                    font-size: 18px;
+                    font-weight: 300; 
+                    margin-bottom: 20px;
+                    margin-top: 10px;
+                    color: #666;
+                    text-align: center;
                 }
 
                 .container-form {
@@ -57,13 +83,9 @@
                     margin-top: 37px;
                 }
                     .customize-input{
-                        text-align: center;
-                        background-color: #101010 !important;
-                        border: none !important;
-                        border-bottom: 1px solid white !important;
-                        border-radius: 0px !important;
-                        box-shadow: 0 0 0 0px !important;
-                        color: white !important;
+                        border-radius: 0 !important;
+                        margin: auto;
+                        width: 308px !important;
                     }
 
                     .center {
@@ -73,8 +95,12 @@
                      button.btn.btn-primary.customize-button{
                          background: #ecf0f1;
                          border: none;
-                         width: 327px;
-                         color: black;
+                         width: 308px !important;
+                         color: white;
+                         background-color: #e64226;
+                         position: relative;
+                         top: 29px;
+                         height: 40px;
                     }
 
 </style>
@@ -85,13 +111,16 @@
     <div class="inner-container-login">
         <div class="body-container">
             <div class="img-logo-company"></div>
+            <div class="title">One account. Access all services.</div>
+            <div class="bdre2"></div>
+            <div class="title2">Sign In to access Accounts</div>
             <div class="container-form">
                 <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
 
                     {{ csrf_field() }}
 
                     <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <input id="email" type="email" class="form-control customize-input" name="email" value="{{ old('email') }}" placeholder="Username" required autofocus>
+                        <input id="email" type="email" class="form-control customize-input" name="email" value="{{ old('email') }}" placeholder="Email" required autofocus>
                         @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -111,7 +140,7 @@
 
                     <div class="form-group center">
                         <button type="submit" class="btn btn-primary customize-button" id="quickstart-sign-in">
-                            Login
+                            Sign In
                         </button>
                     </div>
 
