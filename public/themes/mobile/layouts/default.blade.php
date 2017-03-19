4a053f7f177e0484708f14d3221fe8a7ef45a60a
roialdrin.macuana <woopacumana@gmail.com>
2017-03-18 19:10:20 -0700

    commit default bugfix

--- public/themes/mobile/layouts/default.blade.php
+++ public/themes/mobile/layouts/default.blade.php
@@ -13,16 +13,39 @@
         {!! Theme::asset()->scripts() !!}
     </head>
     <body>
-
-    <div class="views">
-
-      <div class="view view-main">
-                {!! Theme::content() !!}
-      </div>
-
-      <div class="toolbar tabbar tabbar-labels">
-        <div class="toolbar-inner"><a href="/dashboard" class="tab-link active"> <i class="icon tabbar-demo-icon-1"></i><span class="tabbar-label">Dashboard</span></a><a href="/productout" class="tab-link"><i class="icon tabbar-demo-icon-2"></i><span class="tabbar-label">Products</span></a><a href="/user" class="tab-link"> <i class="icon tabbar-demo-icon-3"><span class="badge bg-red">4</span></i><span class="tabbar-label">Users</span></a><a href="#view-4" class="tab-link"> <i class="icon tabbar-demo-icon-4"></i><span class="tabbar-label">Reports</span></a></div>
-      </div>
+    <!-- Status bar overlay for fullscreen mode-->
+    <div class="statusbar-overlay"></div>
+    <!-- Panels overlay-->
+    <div class="panel-overlay"></div>
+    <!-- Left panel with reveal effect-->
+    <div class="panel panel-left panel-reveal">
+        <div class="content-block">
+            <p>Left panel content goes here</p>
+        </div>
+    </div>
+    <!-- Right panel with cover effect-->
+    <div class="panel panel-right panel-cover">
+        <div class="content-block">
+            <p>Right panel content goes here</p>
+        </div>
+    </div>
+    <!-- Views, and they are tabs-->
+    <!-- We need to set "toolbar-through" class on it to keep space for our tab bar-->
+    <div class="views tabs toolbar-through">
+        <!-- Your first view, it is also a .tab and should have "active" class to make it visible by default-->
+        <div id="view-1" class="view view-main tab active">
+            <!-- Pages-->
+            <div class="pages">
+                <!-- Page, data-page contains page name-->
+                <div data-page="index-1" class="page">
+                    {!! Theme::content() !!}
+                </div>
+            </div>
+        </div>
+        <!-- Bottom Tabbar-->
+        <div class="toolbar tabbar tabbar-labels">
+            <div class="toolbar-inner"><a href="/dashboard" class="tab-link active"> <i class="icon tabbar-demo-icon-1"></i><span class="tabbar-label">Dashboard</span></a><a href="/productout" class="tab-link"><i class="icon tabbar-demo-icon-2"></i><span class="tabbar-label">Products</span></a><a href="/user" class="tab-link"> <i class="icon tabbar-demo-icon-3"><span class="badge bg-red">4</span></i><span class="tabbar-label">Users</span></a><a href="#view-4" class="tab-link"> <i class="icon tabbar-demo-icon-4"></i><span class="tabbar-label">Reports</span></a></div>
+        </div>
     </div>
     </body>
 </html>
